7e2a1f58263047640253f1fc340e67a902947b8f
JAC <jacvcarrasquilla@gmail.com>
2020-05-11 15:22:27 -0400

    Aplicacion tareas javascript

--- tareas.js
+++ tareas.js
@@ -1,6 +1,7 @@
 (function(){
     // variable
     var lista=document.getElementById("lista"),
+        listaok=document.getElementById("tarealista"),
         agregar=document.getElementById("agregar"), 
         boton=document.getElementById("btn-tarea");
 
@@ -17,6 +18,9 @@
                 nuevoLi.appendChild(nuevoA);
                 nuevoLi.setAttribute("id", tarea);
                 document.getElementById("lista").appendChild(nuevoLi);
+                
+                // gg
+                
                 nuevoInput = document.createElement("input");
                 nuevoInput.setAttribute("type", "button");
                 nuevoInput.setAttribute("value", "Eliminar");
@@ -28,14 +32,25 @@
                 nuevoInput = document.createElement("input");
                 nuevoInput.setAttribute("type", "button");
                 nuevoInput.setAttribute("value", " OK ");
-                nuevoInput.className = "btn-del";
+                nuevoInput.className = "btn-ok";
                 nuevoLi.appendChild(nuevoInput);
 
                 // gg
 
                 let i=lista.children.length-1;
+
+                // gg
+
                 let btndel = document.getElementsByClassName("btn-del");
                 btndel[i].addEventListener("click", eliminar);
+
+                // gg
+
+                let btnok = document.getElementsByClassName("btn-ok");
+                btnok[i].addEventListener("click", ok);
+
+                // gg
+
                 agregar.value = "";
         } else {
             alert("Por favor ingrese tarea a agregar.");
@@ -46,6 +61,16 @@
         let idpadre = this.parentNode.getAttribute("id");
         let padre = document.getElementById(idpadre);
         padre.parentNode.removeChild(padre);
+    };
+
+    var ok = function(){
+        let idpadre = this.parentNode.getAttribute("id");
+        let padre = document.getElementById(idpadre);
+        padre.parentNode.removeChild(padre);
+        document.getElementById("tarealista").appendChild(padre);
+
+        padre = document.getElementById(idpadre);
+        padre.removeChild(padre.lastElementChild);
     };
     
     // eventos
